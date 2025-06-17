6fc4a5b7d1852997ba88c24c963b98a182a86b52
Alex Heinrich <alexander.heinrich@wsu.edu>
2025-06-17 14:58:29 -0700

    converted to OOP

--- GPE.py
+++ GPE.py
@@ -19,13 +19,13 @@ m = 1.443 * 10**(-25) # mass in kg; rubidium-87
 a = 5.290 * 10**(-9)  # scattering length in m
 λ = 804.1 * 10**(-9)  # wavelength in m; rubidium-87 D2
 ω = 2*np.pi * 50      # harmonic trap frequency in Hz
-δ = 0.5               # detuning
-Ω = 0.5               # Raman coupling strength
+δ = -0.5               # detuning
+Ω_0 = 0.5               # Raman coupling strength
 
 ### Dynamic parameters
-n_steps = 1000  # number of x values in the domain
+n_steps = 512  # number of x values in the domain
 x_max   = 1    # width of spatial grid
-dt      = 0.001 # time step
+dt      = 0.01 # time step
 
 ## Automatic rescaling
 ### With units
@@ -46,43 +46,43 @@ g = g_s
 
 #####################################################################################
 # Functions
-def build_bases():
+def _build_bases():
     """ Builds position and momentum values as 1D arrays. """
     
     x  = np.linspace(-x_max, x_max, n_steps, endpoint=False)
     dx = x[1]-x[0]
     p  = 2*π * ℏ * np.fft.fftfreq(n_steps, d=dx)
-    return x, dx, p
+    return [x, dx, p]
 
-def build_kinetic_and_potential():
+def _build_kinetic_and_potential(sys):
     """ Defines potential and calculates operators as 1D arrays. """
     
     # Harmonic trap
-    U_0    = (1/2) * (m * ω**2) * x**2
+    U_0    = (1/2) * (m * ω**2) * sys.x**2
     
     # Zeeman detuning
     U_up   = U_0 + δ/2
     U_down = U_0 - δ/2
     
     # Kinetic energy
-    K = p**2 / (2 * m)
+    K = sys.p**2 / (2 * m)
     
-    return [U_0, U_up, U_down], K
+    return [U_0, U_up, U_down, K]
 
-def build_ansatz():
+def _build_ansatz(sys):
     """ Defines and constructs a guess for the ground state as a 1D array. """
     
     # Gaussian ansatz
-    ψ_up_x   = np.exp(x**2)
-    ψ_down_x = np.exp(x**2)
+    ψ_up_x   = np.exp(sys.x**2)
+    ψ_down_x = np.exp(sys.x**2)
     
     # Normalize
-    ψ_up_x   /= np.sqrt(np.sum(np.abs(ψ_up_x)**2) * dx)
-    ψ_down_x /= np.sqrt(np.sum(np.abs(ψ_down_x)**2) * dx)
+    ψ_up_x   /= np.sqrt(np.sum(np.abs(ψ_up_x)**2) * sys.dx)
+    ψ_down_x /= np.sqrt(np.sum(np.abs(ψ_down_x)**2) * sys.dx)
     
     return np.stack([ψ_up_x, ψ_down_x])
 
-def optimize_ansatz(ψ_x):
+def _optimize_ansatz(sys, Ω=Ω_0):
     """ Optimizes the ansatz with the split-step Fourier method and imaginary-time
         evolution via second-order Strang splitting.
         
@@ -91,16 +91,18 @@ def optimize_ansatz(ψ_x):
         as elements. It can technically be worked with an exponential matrix, but
         the hyperbolic expansion is more efficient for a 2×2 matrix. """
     
+    ψ_x = sys.ψ_x
+    
     # Refine over n_steps
     for _ in tqdm(range(n_steps), desc=f"{'refining ansatz':<35}"):
         ψ_up_x, ψ_down_x = ψ_x
         
         # Half step in momentum space
         ψ_up_k = np.fft.fft(ψ_up_x)
-        ψ_up_k *= np.exp(-(K/ℏ) * (dt/2))
+        ψ_up_k *= np.exp(-(sys.K/ℏ) * (dt/2))
         ψ_up_x = np.fft.ifft(ψ_up_k)
         ψ_down_k = np.fft.fft(ψ_down_x)
-        ψ_down_k *= np.exp(-(K/ℏ) * (dt/2))
+        ψ_down_k *= np.exp(-(sys.K/ℏ) * (dt/2))
         ψ_down_x = np.fft.ifft(ψ_down_k)
         
         # Full-step: potential + Raman coupling
@@ -108,10 +110,10 @@ def optimize_ansatz(ψ_x):
         ψ_new_down = np.zeros_like(ψ_down_x, dtype=complex)
         
         ## Handle each position individually
-        for i in range(len(x)):
+        for i in range(len(sys.x)):
             n_tot = np.abs(ψ_up_x[i])**2 + np.abs(ψ_down_x[i])**2
-            a = U_up[i]   + g * n_tot
-            b = U_down[i] + g * n_tot
+            a = sys.U_up[i]   + g * n_tot
+            b = sys.U_down[i] + g * n_tot
             Δ = a - b
             h = np.sqrt(Δ**2 + Ω**2)
 
@@ -130,35 +132,89 @@ def optimize_ansatz(ψ_x):
         ψ_down_x = ψ_new_down
         
         # Normalize
-        norm = np.sqrt(np.sum(np.abs(ψ_up_x)**2 + np.abs(ψ_down_x)**2) * dx)
+        norm = np.sqrt(np.sum(np.abs(ψ_up_x)**2 + np.abs(ψ_down_x)**2) * sys.dx)
         ψ_up_x   /= norm
         ψ_down_x /= norm
         ψ_x = np.stack([ψ_up_x, ψ_down_x])
     
     return ψ_x
 
+def _phase_separation_metric(sys):
+    ψ_up, ψ_down = sys.ψ_x
+    n_up = np.abs(ψ_up)**2
+    n_down = np.abs(ψ_down)**2
+
+    numerator = np.sum(n_up * n_down) * sys.dx
+    denom = np.sqrt(np.sum(n_up**2) * sys.dx * np.sum(n_down**2) * sys.dx)
+
+    s = 1 - (numerator / denom)
+    return s
+
+class System:
+
+    def __init__(self):
+        
+        # Construct static operators
+        ops     = _build_bases()
+        self.x  = ops[0]
+        self.dx = ops[1]
+        self.p  = ops[2]
+        ops         = _build_kinetic_and_potential(self)
+        self.U_0    = ops[0]
+        self.U_up   = ops[1]
+        self.U_down = ops[2]
+        self.K      = ops[3]
+
+    def plot_density(self):
+        
+        # Construct and optimize ground state
+        self.ψ_x = _build_ansatz(self)
+        self.ψ_x = _optimize_ansatz(self)
+        
+        # Plot the density
+        plt.plot(self.x, np.abs(self.ψ_x[0])**2, label=r'$|\psi_\uparrow(x)|^2$')
+        plt.plot(self.x, np.abs(self.ψ_x[1])**2, label=r'$|\psi_\downarrow(x)|^2$')
+        plt.plot(self.x, self.U_up   / np.max(self.U_0) * np.max(np.abs(self.ψ_x[0])**2), '--', label=r'$U_\uparrow(x)$')
+        plt.plot(self.x, self.U_down / np.max(self.U_0) * np.max(np.abs(self.ψ_x[0])**2), '--', label=r'$U_\downarrow(x)$')
+
+        plt.title('Spinor BEC Ground State (No SOC yet)')
+        plt.xlabel(r'$x/\sqrt{ℏ/mω}$')
+        plt.ylabel(r'$|\psi(x)|^2 \cdot \sqrt{ℏ/mω}$')
+
+        plt.legend(loc='upper right')
+        plt.grid()
+        plt.show()
+
+    def plot_fig_4(self):
+        
+        s_vals = []
+        Ω_list = np.linspace(0, 2, 5)
+        for Ω in tqdm(Ω_list):
+            
+            # Update Ω and rebuild potentials
+            U_up   = 0.5 * m * ω**2 * self.x**2 + δ / 2
+            U_down = 0.5 * m * ω**2 * self.x**2 - δ / 2
+            
+            self.ψ_x = _build_ansatz(self)
+            self.ψ_x = _optimize_ansatz(self, Ω)
+
+            s = _phase_separation_metric(self)
+            s_vals.append(s)
+
+        s_vals = np.array(s_vals)
+        
+        plt.plot(Ω_list, s_vals, '-o', label='Phase separation metric $s$')
+        plt.axvline(0.19, color='gray', linestyle='--', label='Critical $Ω_c$ from Lin et al.')
+        plt.xlabel(r'$Ω / E_L$')
+        plt.ylabel(r'$s$')
+        plt.title('Miscible to Immiscible Transition')
+        plt.grid()
+        plt.legend()
+        plt.show()
+
 #####################################################################################
-# Generate data
-x, dx, p          = build_bases()
-U_list, K         = build_kinetic_and_potential()
-U_0, U_up, U_down = U_list
-
-ψ_x               = build_ansatz()
-ψ_x               = optimize_ansatz(ψ_x)
-
-#####################################################################################
-# Plot data
-plt.plot(x, np.abs(ψ_x[0])**2, label=r'$|\psi_\uparrow(x)|^2$')
-plt.plot(x, np.abs(ψ_x[1])**2, label=r'$|\psi_\downarrow(x)|^2$')
-plt.plot(x, U_up   / np.max(U_0) * np.max(np.abs(ψ_x[0])**2), '--', label=r'$U_\uparrow(x)$')
-plt.plot(x, U_down / np.max(U_0) * np.max(np.abs(ψ_x[0])**2), '--', label=r'$U_\downarrow(x)$')
-
-plt.title('Spinor BEC Ground State (No SOC yet)')
-plt.xlabel(r'$x/\sqrt{ℏ/mω}$')
-plt.ylabel(r'$|\psi(x)|^2 \cdot \sqrt{ℏ/mω}$')
-
-plt.legend(loc='upper right')
-plt.grid()
-plt.show()
+# Generate and plot data
+sys = System()
+sys.plot_fig_4()
 
 #####################################################################################
