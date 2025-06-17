1ecfec4f29cd7dfc589193c342aadf057a1e612b
Alex Heinrich <alexander.heinrich@wsu.edu>
2025-06-17 12:27:16 -0700

    added detuning term

--- GPE.py
+++ GPE.py
@@ -18,6 +18,7 @@ m = 1.443 * 10**(-25) # mass in kg; rubidium-87
 a = 5.290 * 10**(-9)  # scattering length in m
 λ = 804.1 * 10**(-9)  # wavelength in m; rubidium-87 D2
 ω = 2*np.pi * 50      # harmonic trap frequency in Hz
+δ = 0.5               # detuning
 
 ### Dynamic parameters
 n_steps = 1000  # number of x values in the domain
@@ -54,9 +55,11 @@ def build_bases():
 def build_kinetic_and_potential():
     """ Defines potential and calculates operators as 1D arrays. """
     
-    U = (1/2) * (m * ω**2) * x**2
+    U_0    = (1/2) * (m * ω**2) * x**2
+    U_up   = U_0 + δ/2
+    U_down = U_0 - δ/2
     K = p**2 / (2 * m)
-    return U, K
+    return [U_0, U_up, U_down], K
 
 def build_ansatz():
     """ Defines and constructs a guess for the ground state as a 1D array. """
@@ -82,11 +85,10 @@ def optimize_ansatz(ψ_x):
         ψ_down_k *= np.exp(-(K/ℏ) * (dt/2))
         ψ_down_x = np.fft.ifft(ψ_down_k)
         
-        
         # Full step in position space
-        U_g = U + g * (np.abs(ψ_up_x)**2 + np.abs(ψ_down_x)**2)
-        ψ_up_x *= np.exp(-(U_g/ℏ) * dt)
-        ψ_down_x *= np.exp(-(U_g/ℏ) * dt)
+        n_tot = np.abs(ψ_up_x)**2 + np.abs(ψ_down_x)**2
+        ψ_up_x   *= np.exp(-(U_up   + g*n_tot)/ℏ * dt)
+        ψ_down_x *= np.exp(-(U_down + g*n_tot)/ℏ * dt)
         
         # Normalize
         norm = np.sqrt(np.sum(np.abs(ψ_up_x)**2 + np.abs(ψ_down_x)**2) * dx)
@@ -98,17 +100,19 @@ def optimize_ansatz(ψ_x):
 
 #####################################################################################
 # Generate data
-x, dx, p = build_bases()
-U, K     = build_kinetic_and_potential()
+x, dx, p  = build_bases()
+U_list, K = build_kinetic_and_potential()
+U_0, U_up, U_down = U_list
 
-ψ_x      = build_ansatz()
-ψ_x      = optimize_ansatz(ψ_x)
+ψ_x             = build_ansatz()
+ψ_x             = optimize_ansatz(ψ_x)
 
 #####################################################################################
 # Plot data
 plt.plot(x, np.abs(ψ_x[0])**2, label=r'$|\psi_\uparrow(x)|^2$')
 plt.plot(x, np.abs(ψ_x[1])**2, label=r'$|\psi_\downarrow(x)|^2$')
-plt.plot(x, U / np.max(U) * np.max(np.abs(ψ_x[0])**2), '--', label='U(x) scaled')
+plt.plot(x, U_up / np.max(U_0) * np.max(np.abs(ψ_x[0])**2), '--', label=r'$U_\uparrow(x)$')
+plt.plot(x, U_down / np.max(U_0) * np.max(np.abs(ψ_x[0])**2), '--', label=r'$U_\downarrow(x)$')
 
 plt.title('Spinor BEC Ground State (No SOC yet)')
 plt.xlabel(r'$x/\sqrt{ℏ/mω}$')
